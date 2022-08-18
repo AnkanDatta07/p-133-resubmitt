54a83f463e21c43ffcba5c83a1ed8e0163d8e683
Ankan Datta <110249662+AnkanDatta07@users.noreply.github.com>
2022-08-18 18:09:24 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,49 @@
+Status = "";
+Bed = "";
+object = [];
+
+function preload()
+{
+    Bed = loadImage("m.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 350);
+    canvas.position(400, 265);
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(Bed, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    object = results;
+}
+
+function draw() {
+    if (Status != undefined) {
+          image(Bed, 0, 0, 640, 350);
+      for (var i = 0; i < object.length; i++) {
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+  
+        fill(255, 0, 0);
+        percent = floor(object[i].confidence * 100);
+        text(object[i].label + " " + percent + "%", object[i].x + 5, object[i].y + 15);
+        noFill();
+        stroke(255, 0, 0);
+        rect(object[i].x, object[i].y, object[i].width, object[i].height);
+      }
+    }
+  }
---
+++ desk.js
@@ -0,0 +1,48 @@
+Status = "";
+desk = "";
+object = [];
+
+function preload()
+{
+    desk = loadImage("d.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 350);
+    canvas.position(400, 265);
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(desk, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    object = results;
+}
+function draw() {
+    if (sStatus != undefined) {
+          image(desk, 0, 0, 640, 350);
+      for (var i = 0; i < object.length; i++) {
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+  
+        fill(255, 0, 0);
+        percent = floor(object[i].confidence * 100);
+        text(objec[i].label + " " + percent + "%", object[i].x + 5, object[i].y + 15);
+        noFill();
+        stroke(255, 0, 0);
+        rect(object[i].x, object[i].y, object[i].width, object[i].height);
+      }
+    }
+  }
